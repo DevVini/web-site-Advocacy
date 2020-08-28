5a119100d8587d32e4e02c55a83cbadcc9bb2bc6
Vinicius <64176877+DevVini@users.noreply.github.com>
2020-08-28 00:12:06 -0300

    Add files via upload

---
+++ B.V.1.4/js/main.js
@@ -0,0 +1,15 @@
+
+/*function iniciaModal(modalID) {
+    const modal = document.getElementById(modalID);
+    if (modal) {
+        modal.classList.add('mostrar');
+        modal.addEventListener('click', e => {
+            if (e.target.id == modalID || e.target.classList == 'fechar') {
+                modal.classList.remove('mostrar');
+            }
+        });
+    }
+}
+const botao = document.getElementById('botaoContato');
+botao.addEventListener('click', () => iniciaModal('modal-formulario'));
+*/
